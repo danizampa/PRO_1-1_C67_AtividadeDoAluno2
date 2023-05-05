bf5c5b9d1684ef356bb41305a62fc7877317dd58
danizampa <danizampa@gmail.com>
2023-05-05 16:34:34 -0300

    desfiz alteração

--- App.js
+++ App.js
@@ -27,7 +27,7 @@ export default class App extends React.Component {
         <Header
           backgroundColor={'#9c8210'}
           centerComponent={{
-            text: 'Macaquinho Fofo g',
+            text: 'Macaquinho Fofo',
             style: { color: '#fff', fontSize: 20 },
           }}
         />
